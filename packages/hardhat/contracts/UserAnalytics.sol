d736a882caf8919744d6cfb662748a6407e96036
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-05-05 22:47:27 +0530

    Add: recommendation functions

--- packages/hardhat/contracts/UserAnalytics.sol
+++ packages/hardhat/contracts/UserAnalytics.sol
@@ -2,172 +2,369 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract UserAnalytics {
-  uint256[][] public userActivityMatrix;
-  mapping(address => uint256) public addressToId;
-  mapping(uint256 => address) public idToAddress;
-  mapping(address => uint256) public consumerCredits;
-  mapping(bytes32 => uint256) public schemaIndex;
-  uint256 public latestIndex;
-  uint256 public totalCategories;
+	uint256[][] public userActivityMatrix;
+	mapping(address => uint256) public addressToId;
+	mapping(uint256 => address) public idToAddress;
+	mapping(address => uint256) public consumerCredits;
+	mapping(bytes32 => uint256) public schemaIndex;
+	uint256 public latestIndex;
+	uint256 public totalCategories;
 
-  enum Category {
-    Gaming,
-    Marketplace,
-    Defi,
-    Dao,
-    Web3Social,
-    Identity,
-    Certificates
-  }
+	enum Category {
+		Gaming,
+		Marketplace,
+		Defi,
+		Dao,
+		Web3Social,
+		Identity,
+		Certificates
+	}
 
-  struct Analytics {
-    bytes32 schemaName;
-    bytes32[] columns;
-    Category schemaCategory;
-    uint256[][] data;
-    mapping(address => uint256) addressToId;
-    mapping(uint256 => address) idToAddress;
-    mapping(bytes32 => uint256) columnToIndex;
-  }
+	struct Analytics {
+		bytes32 schemaName;
+		bytes32[] columns;
+		Category schemaCategory;
+		uint256[][] data;
+		mapping(address => uint256) addressToId;
+		mapping(uint256 => address) idToAddress;
+		mapping(bytes32 => uint256) columnToIndex;
+	}
 
-  Analytics[] public dappAnalytics;
+	struct similarityPair {
+		uint256 index;
+		uint256 similarity;
+	}
 
-  constructor() {
-    uint256[] memory initialMatrix;
-    userActivityMatrix.push(initialMatrix);
-    latestIndex = 0;
-    totalCategories = 7;
-    dappAnalytics.push();
-  }
+	Analytics[] public dappAnalytics;
 
-  event NewAnalytics(
-    address user,
-    address provider,
-    uint256 category
-  );
+	constructor() {
+		uint256[] memory initialMatrix;
+		userActivityMatrix.push(initialMatrix);
+		latestIndex = 0;
+		totalCategories = 7;
+		dappAnalytics.push();
+	}
 
-  function addUser(address userAddress) external {
-    // get the total length of current activity matrix
-    latestIndex = latestIndex + 1;
-    uint256[] memory initialMatrix;
-    userActivityMatrix.push(initialMatrix);
+	event NewAnalytics(address user, address provider, uint256 category);
 
-    // add the new user details
-    for(uint256 i = 0; i < totalCategories; i++ ) {
-        userActivityMatrix[latestIndex].push(0);
-    }
-    
+	function addUser(address userAddress) external {
+		// get the total length of current activity matrix
+		latestIndex = latestIndex + 1;
+		uint256[] memory initialMatrix;
+		userActivityMatrix.push(initialMatrix);
 
-    // add user id to address mapping
-    addressToId[userAddress] = latestIndex;
-    idToAddress[latestIndex] = userAddress;
-  }
+		// add the new user details
+		for (uint256 i = 0; i < totalCategories; i++) {
+			userActivityMatrix[latestIndex].push(0);
+		}
 
-  function addSchema(bytes32 schemaName, bytes32[] calldata columns, Category category) external {
-    // initializing schema with defaults
-    Analytics storage analytics = dappAnalytics.push();
-    analytics.schemaName = schemaName;
-    analytics.schemaCategory = category;
-    uint256[] memory initialUser;
-    analytics.data.push(initialUser);
-    for(uint256 i = 0; i < columns.length; i++) {
-      analytics.data[0].push(0);
-      analytics.columns.push(columns[i]);
-      analytics.columnToIndex[columns[i]] = i;
-    }
-    
-    // adding to schema index map
-    schemaIndex[schemaName] = dappAnalytics.length - 1;
-  }
+		// add user id to address mapping
+		addressToId[userAddress] = latestIndex;
+		idToAddress[latestIndex] = userAddress;
+	}
 
-  function addAnalytics(address payable userAddress, bytes32 schemaName, bytes32[] calldata columns, uint256[] calldata data ) public payable {
-    
-    require(schemaIndex[schemaName] != 0, "SCHEMA NOT PRESENT");
-    
-    // add user if not already present
-    if (addressToId[userAddress] == 0) {
-      this.addUser(userAddress);
-    }
+	function addSchema(
+		bytes32 schemaName,
+		bytes32[] calldata columns,
+		Category category
+	) external {
+		// initializing schema with defaults
+		Analytics storage analytics = dappAnalytics.push();
+		analytics.schemaName = schemaName;
+		analytics.schemaCategory = category;
+		uint256[] memory initialUser;
+		analytics.data.push(initialUser);
+		for (uint256 i = 0; i < columns.length; i++) {
+			analytics.data[0].push(0);
+			analytics.columns.push(columns[i]);
+			analytics.columnToIndex[columns[i]] = i;
+		}
 
-    // retrieve storage instance
-    Analytics storage schemaAnalytics = dappAnalytics[schemaIndex[schemaName]];
+		// adding to schema index map
+		schemaIndex[schemaName] = dappAnalytics.length - 1;
+	}
 
-    // push new user if not already present
-    if (schemaAnalytics.addressToId[userAddress] == 0) {
-      schemaAnalytics.data.push();
-      for(uint256 i = 0; i < schemaAnalytics.columns.length; i++) {
-        schemaAnalytics.data[schemaAnalytics.data.length - 1].push(0);
-      }
-      
-      schemaAnalytics.addressToId[userAddress] = schemaAnalytics.data.length - 1;
-      schemaAnalytics.idToAddress[schemaAnalytics.data.length - 1] = userAddress;
-    }  
+	function addAnalytics(
+		address payable userAddress,
+		bytes32 schemaName,
+		bytes32[] calldata columns,
+		uint256[] calldata data
+	) public payable {
+		require(schemaIndex[schemaName] != 0, "SCHEMA NOT PRESENT");
 
-    // add to the existing data
-    for(uint256 i = 0; i < columns.length; i++) {
-      schemaAnalytics.data[schemaAnalytics.addressToId[userAddress]][schemaAnalytics.columnToIndex[columns[i]]] += data[i];
-    }
+		// add user if not already present
+		if (addressToId[userAddress] == 0) {
+			this.addUser(userAddress);
+		}
 
-    userActivityMatrix[addressToId[userAddress]][uint256(schemaAnalytics.schemaCategory)] += 1;
+		// retrieve storage instance
+		Analytics storage schemaAnalytics = dappAnalytics[
+			schemaIndex[schemaName]
+		];
 
-    // rewarding the users for sharing data
-    bool sent = userAddress.send(100);
-    require(sent, "Failed to reward user");
+		// push new user if not already present
+		if (schemaAnalytics.addressToId[userAddress] == 0) {
+			schemaAnalytics.data.push();
+			for (uint256 i = 0; i < schemaAnalytics.columns.length; i++) {
+				schemaAnalytics.data[schemaAnalytics.data.length - 1].push(0);
+			}
 
-    // increasing credit limit for provider
-    consumerCredits[msg.sender] = consumerCredits[msg.sender] + 1;
+			schemaAnalytics.addressToId[userAddress] =
+				schemaAnalytics.data.length -
+				1;
+			schemaAnalytics.idToAddress[
+				schemaAnalytics.data.length - 1
+			] = userAddress;
+		}
 
-    emit NewAnalytics(userAddress, msg.sender, uint256(schemaAnalytics.schemaCategory));
-  }
+		// add to the existing data
+		for (uint256 i = 0; i < columns.length; i++) {
+			schemaAnalytics.data[schemaAnalytics.addressToId[userAddress]][
+				schemaAnalytics.columnToIndex[columns[i]]
+			] += data[i];
+		}
 
-  function updateAnalytics(address payable userAddress, bytes32 schemaName, bytes32[] calldata columns, uint256[] calldata data ) public payable {
-    // add user if not already present
-    if (addressToId[userAddress] == 0) {
-      this.addUser(userAddress);
-    }
+		userActivityMatrix[addressToId[userAddress]][
+			uint256(schemaAnalytics.schemaCategory)
+		] += 1;
 
-    // retrieve storage instance
-    Analytics storage schemaAnalytics = dappAnalytics[schemaIndex[schemaName]];
+		// rewarding the users for sharing data
+		bool sent = userAddress.send(10000000000000000);
+		require(sent, "Failed to reward user");
 
-    // push new user if not already present
-    if (schemaAnalytics.addressToId[userAddress] == 0) {
-      schemaAnalytics.data.push();
-      for(uint256 i = 0; i < schemaAnalytics.columns.length; i++) {
-        schemaAnalytics.data[schemaAnalytics.data.length - 1].push(0);
-      }
-      
-      schemaAnalytics.addressToId[userAddress] = schemaAnalytics.data.length - 1;
-      schemaAnalytics.idToAddress[schemaAnalytics.data.length - 1] = userAddress;
-    }  
+		// increasing credit limit for provider
+		consumerCredits[msg.sender] = consumerCredits[msg.sender] + 1;
 
-    // replace the existing user data with new one
-    for(uint256 i = 0; i < columns.length; i++) {
-      schemaAnalytics.data[schemaAnalytics.addressToId[userAddress]][schemaAnalytics.columnToIndex[columns[i]]] = data[i];
-    }
+		emit NewAnalytics(
+			userAddress,
+			msg.sender,
+			uint256(schemaAnalytics.schemaCategory)
+		);
+	}
 
-    userActivityMatrix[addressToId[userAddress]][uint256(schemaAnalytics.schemaCategory)] += 1;
+	function updateAnalytics(
+		address payable userAddress,
+		bytes32 schemaName,
+		bytes32[] calldata columns,
+		uint256[] calldata data
+	) public payable {
+		// add user if not already present
+		if (addressToId[userAddress] == 0) {
+			this.addUser(userAddress);
+		}
 
-    // rewarding the users for sharing data
-    bool sent = userAddress.send(100);
-    require(sent, "Failed to reward user");
+		// retrieve storage instance
+		Analytics storage schemaAnalytics = dappAnalytics[
+			schemaIndex[schemaName]
+		];
 
-    // increasing credit limit for provider
-    consumerCredits[msg.sender] = consumerCredits[msg.sender] + 1;
+		// push new user if not already present
+		if (schemaAnalytics.addressToId[userAddress] == 0) {
+			schemaAnalytics.data.push();
+			for (uint256 i = 0; i < schemaAnalytics.columns.length; i++) {
+				schemaAnalytics.data[schemaAnalytics.data.length - 1].push(0);
+			}
 
-    emit NewAnalytics(userAddress, msg.sender, uint256(schemaAnalytics.schemaCategory));
-  }
+			schemaAnalytics.addressToId[userAddress] =
+				schemaAnalytics.data.length -
+				1;
+			schemaAnalytics.idToAddress[
+				schemaAnalytics.data.length - 1
+			] = userAddress;
+		}
 
-  function getUserActivityMatrix() external view returns(uint256[][] memory) {
-    return userActivityMatrix;
-  }
+		// replace the existing user data with new one
+		for (uint256 i = 0; i < columns.length; i++) {
+			schemaAnalytics.data[schemaAnalytics.addressToId[userAddress]][
+				schemaAnalytics.columnToIndex[columns[i]]
+			] = data[i];
+		}
 
-  function getAnalyticsDataBySchemaName(bytes32 schemaName) external view returns (uint256[][] memory) {
-    return dappAnalytics[schemaIndex[schemaName]].data;
-  }
+		userActivityMatrix[addressToId[userAddress]][
+			uint256(schemaAnalytics.schemaCategory)
+		] += 1;
 
-  function getColumnsOfSchema(bytes32 schemaName) external view returns (bytes32[] memory) {
-    return dappAnalytics[schemaIndex[schemaName]].columns;
-  }
+		// rewarding the users for sharing data
+		bool sent = userAddress.send(10000000000000000);
+		require(sent, "Failed to reward user");
 
-  receive() external payable {}
+		// increasing credit limit for provider
+		consumerCredits[msg.sender] = consumerCredits[msg.sender] + 1;
+
+		emit NewAnalytics(
+			userAddress,
+			msg.sender,
+			uint256(schemaAnalytics.schemaCategory)
+		);
+	}
+
+	function getUserActivityMatrix()
+		external
+		view
+		returns (uint256[][] memory)
+	{
+		return userActivityMatrix;
+	}
+
+	function getAnalyticsDataBySchemaName(
+		bytes32 schemaName
+	) external view returns (uint256[][] memory) {
+		return dappAnalytics[schemaIndex[schemaName]].data;
+	}
+
+	function getColumnsOfSchema(
+		bytes32 schemaName
+	) external view returns (bytes32[] memory) {
+		return dappAnalytics[schemaIndex[schemaName]].columns;
+	}
+
+	function getRecommendedFollowers(
+		address userAddress,
+		uint64 k
+	) external view returns (address[][] memory) {
+		uint256[][] memory similarityMatrix = computeSimilarityMatrix();
+
+		address[][] memory recommendedFollowers = recommendFollowers(
+			addressToId[userAddress],
+			similarityMatrix,
+			k
+		);
+
+		return recommendedFollowers;
+	}
+
+	// Function to calculate cosine similarity between two users
+	function cosineSimilarity(
+		uint256[] memory user1,
+		uint256[] memory user2
+	) internal pure returns (uint256) {
+		uint256 dotProduct = dot(user1, user2);
+		uint256 normUser1 = sqrt(dot(user1, user1));
+		uint256 normUser2 = sqrt(dot(user2, user2));
+		return ((dotProduct * 100) / normUser1) * normUser2;
+	}
+
+	// Function to compute user-user similarity matrix
+	function computeSimilarityMatrix()
+		internal
+		view
+		returns (uint256[][] memory)
+	{
+		uint256 numUsers = userActivityMatrix.length;
+		uint256[][] memory similarityMatrix = new uint256[][](numUsers);
+
+		// similarityMatrix.push();
+		for (uint256 i = 1; i < numUsers; i++) {
+			uint256[] memory row = new uint256[](numUsers);
+			similarityMatrix[i] = row;
+
+			for (uint64 j = 1; j < numUsers; j++) {
+				uint256[] memory user1 = userActivityMatrix[i];
+				uint256[] memory user2 = userActivityMatrix[j];
+				similarityMatrix[i][j] = cosineSimilarity(user1, user2);
+			}
+		}
+
+		return similarityMatrix;
+	}
+
+	// Function to recommend followers for a given user
+	function recommendFollowers(
+		uint256 userIndex,
+		uint256[][] memory similarityMatrix,
+		uint64 k
+	) public view returns (address[][] memory) {
+		similarityPair[] memory similarUsers = new similarityPair[](
+			similarityMatrix[userIndex].length - 1
+		);
+		uint256 idx = 0;
+
+		// Find k most similar users to the target user
+		for (uint256 j = 1; j < similarityMatrix[userIndex].length; j++) {
+			if (j != userIndex) {
+				similarityPair memory row;
+				row.index = j;
+				row.similarity = similarityMatrix[userIndex][j];
+				similarUsers[idx] = row;
+				idx += 1;
+			}
+		}
+
+		// Sort similar users by descending similarity
+		similarUsers = bubbleSort(similarUsers);
+
+		// Recommend followers from the top k similar users
+		address[][] memory recommendedFollowers = new address[][](
+			totalCategories
+		);
+		for (uint64 i = 0; i < k; i++) {
+			uint64 _idx = 0;
+			for (uint256 j = 0; j < totalCategories; j++) {
+				address[] memory followerRow = new address[](k);
+				recommendedFollowers[j] = followerRow;
+				if (userActivityMatrix[similarUsers[i].index][j] > 0) {
+					recommendedFollowers[j][_idx] = idToAddress[
+						similarUsers[i].index
+					];
+					_idx = _idx + 1;
+				}
+			}
+		}
+
+		return recommendedFollowers;
+	}
+
+	/**
+	 * @dev Bubble sort.
+	 */
+	function bubbleSort(
+		similarityPair[] memory similarUsers
+	) internal pure returns (similarityPair[] memory) {
+		uint256 n = similarUsers.length;
+		for (uint256 i = 0; i < n - 1; i++) {
+			for (uint256 j = 0; j < n - i - 1; j++) {
+				if (
+					similarUsers[j].similarity > similarUsers[j + 1].similarity
+				) {
+					(similarUsers[j], similarUsers[j + 1]) = (
+						similarUsers[j + 1],
+						similarUsers[j]
+					);
+				}
+			}
+		}
+		return similarUsers;
+	}
+
+	/**
+	 * @dev Returns the square root of a number.
+	 */
+	function sqrt(uint256 x) internal pure returns (uint256) {
+		uint256 z = (x + 1) / 2;
+		uint256 y = x;
+		while (z < y) {
+			y = z;
+			z = ((x / z) + z) / 2;
+		}
+		return y;
+	}
+
+	/**
+	 * @dev Returns the dot product of two vectors.
+	 */
+	function dot(
+		uint256[] memory x,
+		uint256[] memory y
+	) internal pure returns (uint256) {
+		require(x.length == y.length);
+
+		uint256 output;
+		for (uint256 i = 0; i < x.length; i++) {
+			output = (x[i] * y[i]) + output;
+		}
+
+		return output;
+	}
+
+	receive() external payable {}
 }
---
+++ packages/hardhat/test/UserAnalytics.ts
@@ -0,0 +1,127 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { UserAnalytics } from "../typechain-types";
+import { encodeBytes32String } from "ethers";
+
+describe("UserAnalytics", function () {
+  // We define a fixture to reuse the same setup in every test.
+
+  let UserAnalytics: UserAnalytics;
+
+  // const [owner, dapp1, dapp2, dapp3, dapp4, dapp5, user1, user2, user3, user4, user5] = await ethers.getSigners();
+
+  before(async () => {
+    const userAnalyticsFactory = await ethers.getContractFactory("UserAnalytics");
+    UserAnalytics = (await userAnalyticsFactory.deploy()) as UserAnalytics;
+    await UserAnalytics.waitForDeployment();
+  });
+
+  describe("Schema", function () {
+    it("Should be able to create a new schema", async function () {
+      const schemaName = encodeBytes32String("schema1");
+      const column1 = encodeBytes32String("col1");
+      const column2 = encodeBytes32String("col2");
+      const column3 = encodeBytes32String("col3");
+
+      await UserAnalytics.addSchema(schemaName, [column1, column2, column3], 0n);
+
+      const schemaIndex = await UserAnalytics.schemaIndex(schemaName);
+
+      expect(await UserAnalytics.dappAnalytics(schemaIndex)).to.deep.equal([schemaName, 0n]);
+      expect(await UserAnalytics.getColumnsOfSchema(schemaName)).to.deep.equal([column1, column2, column3]);
+    });
+  });
+
+  describe("AddAnalytics", function () {
+    it("Should be able to add new user's analytics", async function () {
+      const schemaName = encodeBytes32String("schema1");
+      const column1 = encodeBytes32String("col1");
+      const column2 = encodeBytes32String("col2");
+      const column3 = encodeBytes32String("col3");
+
+      const [owner, dapp1, user1] = await ethers.getSigners();
+
+      const initialUserBalance = await ethers.provider.getBalance(user1);
+
+      await UserAnalytics.connect(owner).addSchema(schemaName, [column1, column2, column3], 0n);
+
+      await UserAnalytics.connect(dapp1).addAnalytics(user1.address, schemaName, [column1], [1n], {
+        value: 10000000000000000n,
+      });
+
+      const userId = await UserAnalytics.addressToId(user1.address);
+
+      expect(await UserAnalytics.userActivityMatrix(userId, 0n)).to.deep.equal(1n);
+      expect(await UserAnalytics.getAnalyticsDataBySchemaName(schemaName)).to.deep.equal([
+        [0, 0, 0],
+        [1, 0, 0],
+      ]);
+
+      const finalUserBalance = await ethers.provider.getBalance(user1);
+
+      expect(finalUserBalance - initialUserBalance).to.equal(10000000000000000n);
+
+      expect(await UserAnalytics.consumerCredits(dapp1.address)).to.equal(1);
+    });
+  });
+
+  describe("Recommendations", function () {
+    it("Should return recommended users", async function () {
+      const schemaName1 = encodeBytes32String("schema1");
+      const schema1Column1 = encodeBytes32String("s1col1");
+      const schema1Column2 = encodeBytes32String("s1col2");
+      const schema1Column3 = encodeBytes32String("s1col3");
+
+      const schemaName2 = encodeBytes32String("schema2");
+      const schema2Column1 = encodeBytes32String("s2col1");
+      const schema2Column2 = encodeBytes32String("s2col2");
+      const schema2Column3 = encodeBytes32String("s2col3");
+
+      const schemaName3 = encodeBytes32String("schema3");
+      const schema3Column1 = encodeBytes32String("s3col1");
+      const schema3Column2 = encodeBytes32String("s3col2");
+      const schema3Column3 = encodeBytes32String("s3col3");
+
+      const schemaName4 = encodeBytes32String("schema4");
+      const schema4Column1 = encodeBytes32String("s4col1");
+      const schema4Column2 = encodeBytes32String("s4col2");
+      const schema4Column3 = encodeBytes32String("s4col3");
+
+      const [dapp1, dapp2, dapp3, dapp4, user1, user2, user3, user4] = await ethers.getSigners();
+
+      await UserAnalytics.connect(dapp1).addSchema(schemaName1, [schema1Column1, schema1Column2, schema1Column3], 0n);
+
+      await UserAnalytics.connect(dapp2).addSchema(schemaName2, [schema2Column1, schema2Column2, schema2Column3], 1n);
+
+      await UserAnalytics.connect(dapp3).addSchema(schemaName3, [schema3Column1, schema3Column2, schema3Column3], 0n);
+
+      await UserAnalytics.connect(dapp4).addSchema(schemaName4, [schema4Column1, schema4Column2, schema4Column3], 1n);
+
+      await UserAnalytics.connect(dapp1).addAnalytics(user1.address, schemaName1, [schema1Column1], [1n], {
+        value: 10000000000000000n,
+      });
+
+      await UserAnalytics.connect(dapp2).addAnalytics(user2.address, schemaName2, [schema2Column1], [1n], {
+        value: 10000000000000000n,
+      });
+
+      await UserAnalytics.connect(dapp3).addAnalytics(user3.address, schemaName3, [schema3Column1], [1n], {
+        value: 10000000000000000n,
+      });
+
+      await UserAnalytics.connect(dapp4).addAnalytics(user4.address, schemaName4, [schema4Column1], [1n], {
+        value: 10000000000000000n,
+      });
+
+      expect(await UserAnalytics.getRecommendedFollowers(user1.address, 1)).to.deep.equal([
+        ["0x0000000000000000000000000000000000000000"],
+        [user2.address],
+        ["0x0000000000000000000000000000000000000000"],
+        ["0x0000000000000000000000000000000000000000"],
+        ["0x0000000000000000000000000000000000000000"],
+        ["0x0000000000000000000000000000000000000000"],
+        ["0x0000000000000000000000000000000000000000"],
+      ]);
+    });
+  });
+});
